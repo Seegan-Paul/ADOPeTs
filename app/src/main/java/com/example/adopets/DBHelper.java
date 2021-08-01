ad6805b30de79690c1afbe7c608a40a6cb9108ff
Seegan-Paul <76612941+Seegan-Paul@users.noreply.github.com>
2021-08-01 19:02:42 +0530

    ADOPeTs1

---
+++ app/src/main/java/com/example/adopets/DBHelper.java
@@ -0,0 +1,47 @@
+package com.example.adopets;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public DBHelper(Context context) {
+        super(context, "Userdata.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create Table Login(serial INTEGER PRIMARY KEY AUTOINCREMENT,fname TEXT,uname TEXT,phone TEXT,email TEXT,password TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists Login");
+    }
+
+    public Boolean insert(String fname,String uname,String phone,String email,String password){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("fname",fname);
+        contentValues.put("uname",uname);
+        contentValues.put("phone",phone);
+        contentValues.put("email",email);
+        contentValues.put("password",password);
+        long result=db.insert("Login",null,contentValues);
+        if(result==-1)
+            return false;
+        else
+            return true;
+    }
+
+    public Cursor getData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("Select * from Login",null);
+        return cursor;
+    }
+}
--- app/src/main/java/com/example/adopets/SignUp.java
+++ app/src/main/java/com/example/adopets/SignUp.java
@@ -1,8 +1,10 @@
 package com.example.adopets;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,8 +14,9 @@ import android.widget.Toast;
 import java.util.regex.Pattern;
 
 public class SignUp extends AppCompatActivity {
-    EditText username,password,password1;
+    EditText username,password,password1,fname,email,phone;
     Button signup;
+    DBHelper db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,13 +24,22 @@ public class SignUp extends AppCompatActivity {
         username=(EditText)findViewById(R.id.editTextTextPersonName2);
         password=(EditText)findViewById(R.id.editTextTextPassword2);
         password1=(EditText)findViewById(R.id.editTextTextPassword3);
+        fname=(EditText)findViewById(R.id.editTextTextPersonName);
+        email=(EditText)findViewById(R.id.editTextTextEmailAddress);
+        phone=(EditText)findViewById(R.id.editTextPhone);
         signup=(Button)findViewById(R.id.button);
+
+        db=new DBHelper(this);
     }
 
     public void signup(View view) {
         String user=username.getText().toString();
         String pwd=password.getText().toString();
         String pwd1=password1.getText().toString();
+        String Fname=fname.getText().toString();
+        String Email=email.getText().toString();
+        String Phone=phone.getText().toString();
+
         if(!isValidPassword(pwd)){
             Toast.makeText(SignUp.this,"Password doesn't match",Toast.LENGTH_SHORT).show();
             return;
@@ -38,6 +50,12 @@ public class SignUp extends AppCompatActivity {
             Toast.makeText(SignUp.this,"Type the correct password",Toast.LENGTH_SHORT).show();
             return;
         }
+
+        Boolean insertData = db.insert(Fname,user,Phone,Email,pwd);
+        if(insertData==true)
+            Toast.makeText(SignUp.this,"Data inserted",Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(SignUp.this,"Data not inserted",Toast.LENGTH_SHORT).show();
 
         Intent intent=new Intent(SignUp.this,MainActivity.class);
         intent.putExtra("username",user);
@@ -63,5 +81,29 @@ public class SignUp extends AppCompatActivity {
          //   return false;
 
         return true;
+    }
+    // Only to View database........
+    public void view(View view) {
+        Cursor res = db.getData();
+        if(res.getCount()==0) {
+            Toast.makeText(SignUp.this, "Table is empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        while(res.moveToNext()){
+            buffer.append("Serial :"+res.getString(0)+"\n");
+            buffer.append("Name :"+res.getString(1)+"\n");
+            buffer.append("UserName :"+res.getString(2)+"\n");
+            buffer.append("Phone :"+res.getString(3)+"\n");
+            buffer.append("Email :"+res.getString(4)+"\n");
+            buffer.append("Password :"+res.getString(5)+"\n\n");
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(SignUp.this);
+        builder.setCancelable(true);
+        builder.setTitle("Login Details");
+        builder.setMessage(buffer.toString());
+        builder.show();
     }
 }
